f86e9aa21e016028630fab8b6cd2f7ad5ef51652
Alassane Ndoye <aloutndoye.an@gmail.com>
2022-01-06 19:01:22 +0000

    update strapi from 4.0.2 to 4.0.3

---
+++ src/api/formateur/controllers/formateur.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  formateur controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::formateur.formateur');
---
+++ src/api/formateur/services/formateur.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * formateur service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::formateur.formateur');
